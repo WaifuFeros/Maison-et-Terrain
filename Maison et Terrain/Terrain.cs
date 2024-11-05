12528627f467786fb64d6b43afcb6691249fd314
WaifuFeros <payet.pep@gmail.com>
2024-11-05 16:11:34 +0100

    Ajout des membres de la classe Terrain

--- Maison et Terrain/Terrain.cs
+++ Maison et Terrain/Terrain.cs
@@ -8,6 +8,27 @@ namespace ExemplePOO
 {
     public class Terrain
     {
-        
+        public string Adresse;
+        public float Superficie;
+        public int NbCotesClotures;
+        public bool Riviere;
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public float EvaluationValeur()
+        {
+            float valeur = 0;
+
+            return valeur;
+        }
+
+        public int CoutFinirCloture()
+        {
+            int cout = 0;
+            return cout;
+        }
     }
 }
