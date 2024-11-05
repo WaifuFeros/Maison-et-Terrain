b9410ae8c77e12ca7909d19b15054965097f039c
WaifuFeros <payet.pep@gmail.com>
2024-11-05 18:49:48 +0100

    Creation classe Piece

---
+++ Maison et Terrain/Piece.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemplePOO
+{
+    public class Piece
+    {
+        public string Nom;
+        public int Superficie;
+
+        public Piece(string Nom, int Superficie)
+        {
+            this.Nom = Nom;
+            this.Superficie = Superficie;
+        }
+    }
+}
