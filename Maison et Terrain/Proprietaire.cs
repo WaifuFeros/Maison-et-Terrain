323921d6aa7bbd618a892953c915a01063cbd542
WaifuFeros <payet.pep@gmail.com>
2024-11-05 19:17:42 +0100

    Ajout de la classe Proprietaire

---
+++ Maison et Terrain/Proprietaire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemplePOO
+{
+    public class Proprietaire
+    {
+        public string Nom;
+        public string Prenom;
+        public List<Bien> Biens;
+
+        public Proprietaire(string Nom, string Prenom, List<Bien> Biens)
+        {
+            this.Nom = Nom;
+            this.Prenom = Prenom;
+            this.Biens = Biens;
+        }
+    }
+}
